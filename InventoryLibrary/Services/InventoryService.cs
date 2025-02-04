423eaa45037f8638143d075c5f23f869a6223cae
tpaing00 <tpaing00@mylangara.ca>
2025-02-03 16:55:15 -0800

    Add project files.

---
+++ InventoryLibrary/Services/InventoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using InventoryLibrary.Models;
+
+namespace InventoryLibrary.Services
+{
+    public class InventoryService
+    {
+        public ObservableCollection<InventoryItem> InventoryItems { get; private set; } = new ObservableCollection<InventoryItem>();
+
+
+    }
+}
